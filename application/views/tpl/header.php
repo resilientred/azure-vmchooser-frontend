e0655e2d3639f466116cd97e3189318d531bc9f9
Karim Vaes <karim@kvaes.be>
2017-07-01 14:07:19 +0200

    Both capabilities can be considered stable. Removed quality tages. Promoted to "Preview" instead of "Private Preview"

--- application/views/tpl/header.php
+++ application/views/tpl/header.php
@@ -20,14 +20,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="<?php echo base_url(); ?>">Azure VM Chooser (Private Preview)</a>
+      <a class="navbar-brand" href="<?php echo base_url(); ?>">Azure VM Chooser (Preview)</a>
     </div>
 	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li><a href="<?php echo base_url()."vmchooser/"; ?>">Search (Stable)</a></li>
+        <li><a href="<?php echo base_url()."vmchooser/"; ?>">Search</a></li>
       </ul>
 	  <ul class="nav navbar-nav">
-        <li><a href="<?php echo base_url()."vmchooser/csv/"; ?>">CSV Upload (Experimental)</a></li>
+        <li><a href="<?php echo base_url()."vmchooser/csv/"; ?>">CSV Upload</a></li>
       </ul>
 	  <ul class="nav navbar-nav navbar-right">
         <li><a href="<?php echo base_url()."vmchooser/about/"; ?>">About</a></li>
