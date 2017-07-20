1c6902a490f4f925600db66e66acc317617eb811
Karim Vaes <karim@kvaes.be>
2017-07-19 21:23:31 -0700

    Changed table contents

--- application/views/vmchooser-vmsize.php
+++ application/views/vmchooser-vmsize.php
@@ -5,11 +5,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <?php
 
-echo "results";
-print_r($results);
 
 if (isset($results)) { 
-  echo "start";
 	?>
 	
 	<div class="page-header">
@@ -29,16 +26,7 @@ if (isset($results)) {
 	$first = true;
 
 	foreach ($results as $result) {
-		$data = array();
-		print_r($result);
-		foreach($result as $key => $value) {
-			$header[] = str_replace("'", "", $key);;
-			$data[] = $value;
-		}
-		if ($first) {
-			$CI->table->set_heading($header);
-		}
-		$CI->table->add_row($data);
+		$CI->table->add_row($result);
 		$first = false;
 	}
 	echo $CI->table->generate();
