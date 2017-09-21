49a89fea4e6c5638b4178f59c5abb8b01ce6c34c
Karim Vaes <kvaes@github.com>
2017-09-21 10:30:03 +0200

    Start from the previous parser summary page for the export... trying to backtrace

--- application/views/vmchooser-results-export.php
+++ application/views/vmchooser-results-export.php
@@ -1,21 +1,42 @@
 <?php
-echo "loading view";
 defined('BASEPATH') OR exit('No direct script access allowed');
+?>
 
-$CI =& get_instance();
 
-echo "go";
+<?php
 
-print_r($results;)
 
-if (isset($results)) {
+if (isset($results)) { 
+	?>
+	
+	<div class="page-header">
+	  <h1 id="navbar">CSV Parser Results</h1>
+	</div>
+	
+	<?php
+
+	$CI =& get_instance();
+	$CI->load->library('table');
+
+	$template = array(
+			'table_open' => '<table class="table table-striped table-hover">'
+	);
+	$CI->table->set_template($template);
 
 	$header = array();
 	foreach($results[0] as $key => $value) {
 		$header[] = $key;
 	}
-	
-	print_r($header);
+
+	$CI->table->set_heading($header);		
+
+	echo $CI->table->generate($results);
+
 }
 
 ?>
+
+<div class="alert alert-dismissible alert-info">
+  <button type="button" class="close" data-dismiss="alert">&times;</button>
+  <strong>Export : </strong> Download results as <a href="<?php echo base_url()."/vmchooser/results/".$csvfile."/csv/"; ?>" target="_blank" class="alert-link">CSV</a> .</br>
+</div>
