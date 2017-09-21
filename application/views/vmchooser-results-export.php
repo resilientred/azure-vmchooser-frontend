603cf86259cae4f8010821865f443d218aa88c05
Karim Vaes <kvaes@github.com>
2017-09-21 11:10:27 +0200

    Insert function

--- application/views/vmchooser-results-export.php
+++ application/views/vmchooser-results-export.php
@@ -5,6 +5,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <?php
 
+function arrayToCsv( array &$fields, $delimiter = ';', $enclosure = '"', $encloseAll = false, $nullToMysqlNull = false ) {
+    $delimiter_esc = preg_quote($delimiter, '/');
+    $enclosure_esc = preg_quote($enclosure, '/');
+
+    $output = array();
+    foreach ( $fields as $field ) {
+        if ($field === null && $nullToMysqlNull) {
+            $output[] = 'NULL';
+            continue;
+        }
+
+        // Enclose fields containing $delimiter, $enclosure or whitespace
+        if ( $encloseAll || preg_match( "/(?:${delimiter_esc}|${enclosure_esc}|\s)/", $field ) ) {
+            $output[] = $enclosure . str_replace($enclosure, $enclosure . $enclosure, $field) . $enclosure;
+        }
+        else {
+            $output[] = $field;
+        }
+    }
+
+    return implode( $delimiter, $output );
+}
+
 if (isset($results)) { 
 	
 	$CI =& get_instance();
