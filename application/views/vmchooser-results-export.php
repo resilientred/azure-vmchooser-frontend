4fa9f18d8da77089057349882b3cc572e97ab943
Karim Vaes <kvaes@github.com>
2017-09-21 10:38:34 +0200

    Revert back... kinda

--- application/views/vmchooser-results-export.php
+++ application/views/vmchooser-results-export.php
@@ -1,6 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+// Kindly borrowed from ;  https://stackoverflow.com/questions/3933668/convert-array-into-csv
 function arrayToCsv( array &$fields, $delimiter = ';', $enclosure = '"', $encloseAll = false, $nullToMysqlNull = false ) {
     $delimiter_esc = preg_quote($delimiter, '/');
     $enclosure_esc = preg_quote($enclosure, '/');
@@ -26,10 +27,22 @@ function arrayToCsv( array &$fields, $delimiter = ';', $enclosure = '"', $enclos
 
 if (isset($results)) { 
 
+	$CI =& get_instance();
+	$CI->load->library('table');
+
+	$template = array(
+			'table_open' => '<table class="table table-striped table-hover">'
+	);
+	$CI->table->set_template($template);
+
 	$header = "array()";
 	foreach($results[0] as $key => $value) {
 		$header[] = $key;
 	}
+
+	$CI->table->set_heading($header);		
+
+	echo $CI->table->generate($results);
 
 	print_r($results);
 
