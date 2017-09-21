879475ad078827ddf83651ae2030e9bd9d2d09e4
Karim Vaes <kvaes@github.com>
2017-09-21 11:04:13 +0200

    Retry... failback

--- application/views/vmchooser-results-export.php
+++ application/views/vmchooser-results-export.php
@@ -1,32 +1,12 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+?>
 
-// Kindly borrowed from ;  https://stackoverflow.com/questions/3933668/convert-array-into-csv
-function arrayToCsv( array &$fields, $delimiter = ';', $enclosure = '"', $encloseAll = false, $nullToMysqlNull = false ) {
-    $delimiter_esc = preg_quote($delimiter, '/');
-    $enclosure_esc = preg_quote($enclosure, '/');
 
-    $output = array();
-    foreach ( $fields as $field ) {
-        if ($field === null && $nullToMysqlNull) {
-            $output[] = 'NULL';
-            continue;
-        }
-
-        // Enclose fields containing $delimiter, $enclosure or whitespace
-        if ( $encloseAll || preg_match( "/(?:${delimiter_esc}|${enclosure_esc}|\s)/", $field ) ) {
-            $output[] = $enclosure . str_replace($enclosure, $enclosure . $enclosure, $field) . $enclosure;
-        }
-        else {
-            $output[] = $field;
-        }
-    }
-
-    return implode( $delimiter, $output );
-}
+<?php
 
 if (isset($results)) { 
-
+	
 	$CI =& get_instance();
 	$CI->load->library('table');
 
@@ -35,7 +15,7 @@ if (isset($results)) {
 	);
 	$CI->table->set_template($template);
 
-	$header = "array()";
+	$header = array();
 	foreach($results[0] as $key => $value) {
 		$header[] = $key;
 	}
@@ -43,8 +23,6 @@ if (isset($results)) {
 	$CI->table->set_heading($header);		
 
 	echo $CI->table->generate($results);
-
-	print_r($results);
 
 }
 
