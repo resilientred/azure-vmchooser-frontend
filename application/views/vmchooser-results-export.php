9e297dd33cd6c4d5a3752ee9cbe3d3eedb0afaef
Karim Vaes <kvaes@github.com>
2017-09-21 11:05:39 +0200

    Retry...

--- application/views/vmchooser-results-export.php
+++ application/views/vmchooser-results-export.php
@@ -8,21 +8,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 if (isset($results)) { 
 	
 	$CI =& get_instance();
-	$CI->load->library('table');
-
-	$template = array(
-			'table_open' => '<table class="table table-striped table-hover">'
-	);
-	$CI->table->set_template($template);
 
 	$header = array();
 	foreach($results[0] as $key => $value) {
 		$header[] = $key;
 	}
 
-	$CI->table->set_heading($header);		
-
-	echo $CI->table->generate($results);
+	print_r($results);
 
 }
 
