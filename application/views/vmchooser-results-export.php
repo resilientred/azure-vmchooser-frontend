faa485e6a482d3913f6ae8207441a5a3093d1c4b
Karim Vaes <kvaes@github.com>
2017-09-21 10:36:33 +0200

    Still moving on

--- application/views/vmchooser-results-export.php
+++ application/views/vmchooser-results-export.php
@@ -1,29 +1,35 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-?>
 
+function arrayToCsv( array &$fields, $delimiter = ';', $enclosure = '"', $encloseAll = false, $nullToMysqlNull = false ) {
+    $delimiter_esc = preg_quote($delimiter, '/');
+    $enclosure_esc = preg_quote($enclosure, '/');
 
-<?php
+    $output = array();
+    foreach ( $fields as $field ) {
+        if ($field === null && $nullToMysqlNull) {
+            $output[] = 'NULL';
+            continue;
+        }
 
+        // Enclose fields containing $delimiter, $enclosure or whitespace
+        if ( $encloseAll || preg_match( "/(?:${delimiter_esc}|${enclosure_esc}|\s)/", $field ) ) {
+            $output[] = $enclosure . str_replace($enclosure, $enclosure . $enclosure, $field) . $enclosure;
+        }
+        else {
+            $output[] = $field;
+        }
+    }
+
+    return implode( $delimiter, $output );
+}
 
 if (isset($results)) { 
 
-	$CI =& get_instance();
-	$CI->load->library('table');
-
-	$template = array(
-			'table_open' => '<table class="table table-striped table-hover">'
-	);
-	$CI->table->set_template($template);
-
-	$header = array();
+	$header = "array()";
 	foreach($results[0] as $key => $value) {
 		$header[] = $key;
 	}
-
-	$CI->table->set_heading($header);		
-
-	echo $CI->table->generate($results);
 
 	print_r($results);
 
